1ea2983a9ff7aa90d4aceb167bc506ae03b58c75
Jiangchanglong1991 <cljiang@bu.edu>
2016-09-20 12:57:45 -0400

    Add files via upload
    add the function of converting pictures to both gray scale and blur

--- BlurImage.cpp
+++ BlurImage.cpp
@@ -1,33 +1,42 @@
 //
-//  main.cpp
-//  opencv_test
+//  GRAYSCALE.cpp
 //
-//  Created by Apple on 15/09/16.
-//  Copyright © 2016 Apple. All rights reserved.
 //
+//  Created by Changlong Jiang on 9/16/16.
+//
+//
+
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core.hpp"
 
 using namespace std;
 using namespace cv;
 
-Mat img; Mat dst;
-Mat b_image;
+Mat src; Mat gray; Mat dst;
 char window_name1[] = "Unprocessed Image";
-char window_name2[] = "Processed Image";
+char window_name2[] = "Blur Image";
+char window_name3[] = "GrayScale Image";
 
 int main( int argc, char** argv )
 {
-    /// Load the source image
-    img = imread( argv[1], 1 );
-    cvtColor(img,dst,CV_BGR2GRAY);
+    src = imread( argv[1], 1 );
+    
     namedWindow( window_name1, WINDOW_AUTOSIZE );
-    imshow("Unprocessed Image",img);
+    imshow("Unprocessed Image",src);
+    
+    dst = src.clone();
+    GaussianBlur( src, dst, Size( 15, 15 ), 0, 0 );
+    
     namedWindow( window_name2, WINDOW_AUTOSIZE );
-    imshow("Processed Image",dst);
-    threshold(dst,b_image,128,255,THRESH_BINARY);
-    namedWindow("Binary image", WINDOW_AUTOSIZE);
-    imshow("Binary image",b_image);
+    imshow("Blur Image",dst);
+    
+    gray.create (src.rows, src.cols, CV_8UC1);
+    cvtColor(src, gray, CV_BGR2GRAY);
+    
+    namedWindow( window_name3, WINDOW_AUTOSIZE );
+    imshow("GrayScale Image",gray);
+    
     waitKey();
     return 0;
 }
